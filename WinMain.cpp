c1d0384a7377695eb087d5b831def70d0e57b70e
光太郎 佐藤 <230011@jc-21.jp>
2024-05-20 15:10:13 +0900

    実際にウィンドウを出した&定数化や文字変更&大きさ変更

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,4 +1,10 @@
 #include <Windows.h>
+#include<tchar.h>
+
+const wchar_t * WIN_CLASS_NAME = L"SampleGame";
+const wchar_t* APP_NAME = L"Hello Windows!!";
+const int WINDOW_WIDTH = 800;  //ウィンドウの幅
+const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
 
 //プロトタイプ宣言
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
@@ -6,15 +12,63 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 //エントリーポイント
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
- //ウィンドウクラス（設計図）を作成
+    WNDCLASSEX wc;
+    wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
+    wc.hInstance = hInstance;                   //インスタンスハンドル
+    wc.lpszClassName = WIN_CLASS_NAME;            //ウィンドウクラス名
+    wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
+    wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
+    wc.hIcon = LoadIcon(NULL, IDI_ERROR); //アイコン
+    wc.hIconSm = LoadIcon(NULL, IDI_WINLOGO);   //小さいアイコン
+    wc.hCursor = LoadCursor(NULL, IDC_WAIT);   //マウスカーソル
+    wc.lpszMenuName = NULL;                     //メニュー（なし）
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH); //背景（白）
+    RegisterClassEx(&wc); //クラスを登録
 
- //ウィンドウを作成
+    //ウィンドウサイズの計算(表示領域をWINDOW_WIDTH x WINDOW_HEIGHTに指定するための計算)
+    RECT winRect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+    AdjustWindowRect(&winRect, WS_OVERLAPPEDWINDOW, FALSE);
+    int winW = winRect.right - winRect.left;     //ウィンドウ幅
+    int winH = winRect.bottom - winRect.top;     //ウィンドウ高さ
 
- //ウィンドウを表示
 
- //メッセージループ（何か起きるのを待つ）
+    HWND hWnd = CreateWindow(
+        WIN_CLASS_NAME,         //ウィンドウクラス名
+        APP_NAME,     //タイトルバーに表示する内容
+        WS_OVERLAPPEDWINDOW, //スタイル（普通のウィンドウ）
+        CW_USEDEFAULT,       //表示位置左（おまかせ）
+        CW_USEDEFAULT,       //表示位置上（おまかせ）
+        winW,        //ウィンドウ幅
+        winH,       //ウィンドウ高さ
+        NULL,                //親ウインドウ（なし）
+        NULL,                //メニュー（なし）
+        hInstance,           //インスタンス
+        NULL                 //パラメータ（なし）
+    );
 
+    ShowWindow(hWnd, nCmdShow);
 
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
+    while (msg.message != WM_QUIT)
+    {
+        //メッセージあり
+        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        //メッセージなし
+        else
+        {
+            //ゲームの処理
+            
+
+        }
+    }
 	return 0;
 }
 
@@ -24,6 +78,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     switch (msg)
     {
     case WM_DESTROY:
+
         PostQuitMessage(0);  //プログラム終了
         return 0;
     }
