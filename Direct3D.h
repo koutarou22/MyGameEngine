69bb9902e6672066d2cf3c4e37472feb7f30817a
光太郎 佐藤 <230011@jc-21.jp>
2024-05-21 15:07:38 +0900

    3D描画『処理を分ける』&『シェーダー』のファイル追加

---
+++ Direct3D.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <d3d11.h>
+
+//リンカ
+#pragma comment(lib, "d3d11.lib")
+
+namespace Direct3D
+{
+	const int WINDOW_WIDTH = 800;  //ウィンドウの幅
+    const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
+
+
+	//初期化
+	void Initialize(int winW, int winH, HWND hWnd);
+
+	//描画開始
+	void BeginDraw();
+
+	//描画終了
+	void EndDraw();
+
+	//解放
+	void Release();
+};
